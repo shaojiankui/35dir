0183a05e0043d221b72cf408091970d80c11d01e
shaojiankui <6077782@qq.com>
2014-08-16 23:59:43 +0800

    Revert "移除搜索xss漏洞"
    This reverts commit 248746eceb7d87851d8449269d61de18e8a1f27e.

--- module/search.php
+++ module/search.php
@@ -20,7 +20,6 @@ if ($curpage > 1) {
 		
 $strtype = strtolower(trim($_GET['type']));
 $keyword = addslashes(trim($_GET['query']));
-$keyword = RemoveXSS($keyword);
 
 if (!$smarty->isCached($tempfile)) {
 	$where = "w.web_status=3";
